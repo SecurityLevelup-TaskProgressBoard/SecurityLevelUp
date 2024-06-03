f177257fa0658275c6d94db223c8ea3239800ffb
SamanthaVS-BBD <157207389+SamanthaVS-BBD@users.noreply.github.com>
2024-06-03 23:27:09 +0200

    removed check token stuff front login

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -21,29 +21,5 @@ function checkInfoParams(){
     }
 }
 
-//can be removed\\
-// function getTokens() {
-//     // Check if tokens are in the URL fragment
-//     const hashParams = new URLSearchParams(window.location.hash.substring(1));
-//     const idToken = hashParams.get('id_token');
-//     const accessToken = hashParams.get('access_token');
-
-//     if (idToken && accessToken) {
-//         // Save tokens to session storage
-//         sessionStorage.setItem('idToken', idToken);
-//         sessionStorage.setItem('accessToken', accessToken);
-
-//         console.log(sessionStorage.getItem('idToken'));
-//         console.log(sessionStorage.getItem('accessToken'));
-//         // Remove tokens from URL
-//         window.history.replaceState({}, document.title, window.location.pathname);
-
-//         // Redirect to a different page
-//         // window.location.href = 'https://localhost:5500/Frontend/index.html';
-//     }
-// }
-
-
-//document.addEventListener('DOMContentLoaded', getTokens);
 document.addEventListener('DOMContentLoaded', checkInfoParams);
 document.getElementById('login-button').addEventListener('click', login);
