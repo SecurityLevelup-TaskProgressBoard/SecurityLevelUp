b5e7e430edbd9f57694adb222830cdc5a67fc9c7
tristanphips <tristan.phipson@bbd.co.za>
2024-06-05 07:41:58 +0200

    added docker

---
+++ docker_db_init.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE taskifydb;
+GO
+
+USE taskifydb;
+GO
+
+CREATE TABLE [Users] (
+  [UserId] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  [UserName] nvarchar(255) NOT NULL
+);
+GO
+
+CREATE TABLE [Boards] (
+  [BoardId] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  [Status] nvarchar(255) NOT NULL
+);
+GO
+
+CREATE TABLE [Tasks] (
+  [TaskId] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  [UserId] int NOT NULL,
+  [BoardId] int NOT NULL,
+  [TaskName] nvarchar(50) NOT NULL,
+  [TaskDescription] nvarchar(200) NOT NULL,
+  [Date] Datetime NOT NULL,
+  [Deleted] bit NOT NULL,
+  FOREIGN KEY ([UserId]) REFERENCES Users([UserId]),
+  FOREIGN KEY ([BoardId]) REFERENCES Boards([BoardId])
+);
+GO
+
+INSERT INTO Boards VALUES
+('TODO'),
+('IN PROGRESS'),
+('DONE');
+GO
+
+USE master;
+GO
